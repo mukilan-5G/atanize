1cebc6e9d2b999aeaf468f3350ef7d61de8b3ecb
mukilan-5g <mukilan@batonsystems.com>
2018-06-02 14:49:42 +0530

    Initial Commit

---
+++ attendance/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from employee.models import Employee
+
+# Create your models here.
+class Attendance(models.Model):
+    date = models.DateField()
+    employee = models.ForeignKey(Employee)
+    is_present = models.BooleanField(default=False)
+    advance = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = "Attendance"
+        unique_together = ('date', 'employee', )
---
+++ employee/models.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class TypeOfWork(models.Model):
+    type = models.CharField(max_length=60)
+    base_salary = models.IntegerField()
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField()
+    created_by = models.ForeignKey(User)
+
+    class Meta:
+        db_table = "TypeOfWork"
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=60)
+    phone_no = models.CharField(max_length=60)
+    address = models.TextField()
+    is_active = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField()
+    created_by = models.ForeignKey(User)
+    type_of_work = models.ForeignKey(TypeOfWork)
+
+    class Meta:
+        db_table = "Employee"
